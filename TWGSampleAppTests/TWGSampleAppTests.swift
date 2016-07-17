78bb54095f60574cf65ba676bd8718f40f6e5250
ArshAulakh59 <arshaulakh@thebhouse.com>
2016-07-17 15:34:06 +0530

    Added LaunchScreen Image.

--- TWGSampleAppTests/TWGSampleAppTests.swift
+++ TWGSampleAppTests/TWGSampleAppTests.swift
@@ -9,28 +9,57 @@
 import XCTest
 @testable import TWGSampleApp
 
+//MARK:- Interface
 class TWGSampleAppTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+    let appDelegateObject = AppDelegate()
+	let picturesControllerObject = PicturesController()
+}
+
+//MARK:- Implementation
+extension TWGSampleAppTests {
+	//MARK: System
+	override func setUp() {
+		super.setUp()
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+	
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		super.tearDown()
+	}
+}
+
+extension TWGSampleAppTests {
+	//MARK: Tests
+	//Test First Launch And Onboarding
+	func testFirstLaunch() {
+		if NSUserDefaults.standardUserDefaults().boolForKey("launchedBefore") {
+			XCTAssertTrue(appDelegateObject.isFirstLaunch, "Is Not First Time Launch")
+		} else {
+			XCTAssertFalse(appDelegateObject.isFirstLaunch, "Is First Time Launch")
+		}
+	}
+	
+	func testOnboarding() {
+		if appDelegateObject.isFirstLaunch {
+			
+		} else {
+			XCTAssertNil("", "")
+		}
+	}
+	
+	//Test Gallery
+	func testNumberOfPicturesInGallery() {
+		
+	}
+}
+
+extension TWGSampleAppTests {
+	//MARK: Performance Test
+	func sampleTestPerformanceMethod() {
+		// This is an example of a performance test case.
+		self.measureBlock {
+			// Put the code you want to measure the time of here.
+		}
+	}
 }
--- TWGSampleAppUITests/TWGSampleAppUITests.swift
+++ TWGSampleAppUITests/TWGSampleAppUITests.swift
@@ -9,12 +9,9 @@
 import XCTest
 
 class TWGSampleAppUITests: XCTestCase {
-        
     override func setUp() {
         super.setUp()
-        
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
@@ -31,6 +28,6 @@ class TWGSampleAppUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+		
     }
-    
 }
